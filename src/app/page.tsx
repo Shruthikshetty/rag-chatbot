5b048833a32afdfa2d7d629849087c02d168ab9b
shruthik shetty <shrutikshetty14@gmail.com>
2026-02-20 18:59:00 +0530

    simple chat set up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,94 @@
 "use client";
 
+import { useState } from "react";
+import {
+  Conversation,
+  ConversationContent,
+  ConversationScrollButton,
+} from "@/components/ai-elements/conversation";
 import { Spinner } from "@/components/ui/spinner";
+import {
+  Message,
+  MessageContent,
+  MessageResponse,
+} from "@/components/ai-elements/message";
+import {
+  PromptInput,
+  PromptInputBody,
+  PromptInputFooter,
+  PromptInputMessage,
+  PromptInputSubmit,
+  PromptInputTextarea,
+  PromptInputTools,
+} from "@/components/ai-elements/prompt-input";
+import { useChat } from "@ai-sdk/react";
+import { Bot, User } from "lucide-react";
 
 export default function Home() {
-  return <div>hello</div>;
+  const [input, setInput] = useState("");
+  // hook to manage the chat state
+  const { messages, sendMessage, status } = useChat();
+
+  // handles the submit
+  const handleSubmit = (message: PromptInputMessage) => {
+    if (!message.text.trim()) return;
+    sendMessage({
+      text: message.text,
+    });
+    // clear our input
+    setInput("");
+  };
+
+  return (
+    <div className="max-w-4xl mx-auto  p-6 relative size-full h-screen ">
+      <div className="flex flex-col h-full">
+        {/* all the chat conversations */}
+        <Conversation className="h-full">
+          <ConversationContent>
+            {messages.map((message) => (
+              <Message key={message.id} from={message.role}>
+                <MessageContent>
+                  {message.role === "assistant" ? (
+                    <Bot className="text-foreground" />
+                  ) : null}
+                  {message.parts.map((part, index) => {
+                    switch (part.type) {
+                      case "text":
+                        return (
+                          <MessageResponse key={`${message.id}-${index}`}>
+                            {part.text}
+                          </MessageResponse>
+                        );
+                      default:
+                        return null;
+                    }
+                  })}
+                </MessageContent>
+              </Message>
+            ))}
+            {status === "streaming" || status === "submitted" ? (
+              <Spinner />
+            ) : null}
+          </ConversationContent>
+          <ConversationScrollButton />
+        </Conversation>
+
+        {/* User input area */}
+        <PromptInput className="mt-4" onSubmit={handleSubmit}>
+          <PromptInputBody>
+            <PromptInputTextarea
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+          </PromptInputBody>
+          <PromptInputFooter>
+            <PromptInputTools>
+              {/* Model selector , web search etc */}
+            </PromptInputTools>
+            <PromptInputSubmit disabled={!input} status={status} />
+          </PromptInputFooter>
+        </PromptInput>
+      </div>
+    </div>
+  );
 }
