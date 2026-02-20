7bc80130ca1cfaedf99bb810f21c5f9e30d2c787
shruthik shetty <shrutikshetty14@gmail.com>
2026-02-20 16:58:48 +0530

    add required components

---
+++ src/app/api/route.ts
@@ -0,0 +1,43 @@
+import { streamText, UIMessage, convertToModelMessages } from "ai";
+import { openai } from "@ai-sdk/openai";
+import { success } from "zod/v4";
+
+export async function POST(req: Request) {
+  // get  messages
+  const { messages = [] }: { messages: UIMessage[] } = await req.json();
+
+  // check if messages are empty
+  if (messages.length === 0) {
+    return Response.json(
+      {
+        message: "Please provide messages",
+        success: false,
+      },
+      {
+        status: 404,
+      },
+    );
+  }
+
+  try {
+    // stream text
+    const result = streamText({
+      model: openai("gpt-4.1-mini"),
+      messages: await convertToModelMessages(messages),
+    });
+
+    return result.toUIMessageStreamResponse();
+  } catch (error) {
+    console.log(error);
+    return Response.json(
+      {
+        message:
+          (error as Error)?.message ?? "Something went wrong try again later",
+        success: false,
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+}
